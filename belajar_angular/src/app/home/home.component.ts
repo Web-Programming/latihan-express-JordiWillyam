4a33df05ead278b4bae932d5c847f35e9a57ea5a
JordiWillyam <jordiwillyamadikusuma_2226240138@mhs.mdp.ac.id>
2024-11-13 20:23:50 +0700

    ADD: housing dan gambar

--- belajar_angular/src/app/home/home.component.ts
+++ belajar_angular/src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -10,6 +12,29 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  housingLocationList: HousingLocation[]=[
+    {
+    id: 0,
+    name: "Tulus",
+    city: "Palembang",
+    state: "Sumsel",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT778q7RjEtGzcQo6zbH6297O2HvEsSc66kDA&s",
+    avaliableUnits: 1,
+    wifi: true,
+    laundry: true,
+    },
+
+    {
+    id: 1,
+    name: "Land Land",
+    city: "Palembang",
+    state: "Sumsel",
+    photo: "https://nowpalembang.com/wp-content/uploads/2021/04/perumahan-palembang.jpg",
+    avaliableUnits: 12,
+    wifi: true,
+    laundry: false,
+    }
+  ];
 }
--- belajar_angular/src/app/housing-location/housing-location.component.ts
+++ belajar_angular/src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section>
+      <img class="listing-photo" [src]="housingLocation.photo" alt="{{housingLocation.name}}">
+      <h2 class="listing-heading">{{housingLocation.name}}</h2>
+      <p class="listing-location">{{housingLocation.city}}, {{housingLocation.state}}</p>
+    </section>
   `,
   styleUrl: './housing-location.component.css'
 })
-export class HousingLocationComponent {
 
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
 }
