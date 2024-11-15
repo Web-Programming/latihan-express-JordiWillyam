013ea8cf99f2d34176a5aad782a433751fb7b5a4
JordiWillyam <jordiwillyamadikusuma_2226240138@mhs.mdp.ac.id>
2024-11-15 17:40:36 +0700

    ADD: detail & CSS

--- belajar_angular/src/app/app.routes.ts
+++ belajar_angular/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-export const routes: Routes = [{ path: 'home', component: HomeComponent , title: 'Home Page'  }];
+import { DetailsComponent } from './details/details.component';
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent, title: 'Home Page' },
+  { path: 'details', component: DetailsComponent, title: 'Details Page' },
+];
--- belajar_angular/src/app/housing-location/housing-location.component.ts
+++ belajar_angular/src/app/housing-location/housing-location.component.ts
@@ -1,21 +1,28 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housing-location';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <section>
-      <img class="listing-photo" [src]="housingLocation.photo" alt="{{housingLocation.name}}">
-      <h2 class="listing-heading">{{housingLocation.name}}</h2>
-      <p class="listing-location">{{housingLocation.city}}, {{housingLocation.state}}</p>
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="{{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+      <a routerLink="/details">Show More </a>
     </section>
   `,
-  styleUrl: './housing-location.component.css'
+  styleUrl: './housing-location.component.css',
 })
-
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
 }
